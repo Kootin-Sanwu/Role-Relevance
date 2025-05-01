742284a9ba364e3bdef35e6be969f1a9b311d96d
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2025-05-01 03:05:59 +0000

    "Removed favicon and apple-touch-icon links from frontend/index.php"

--- frontend/index.php
+++ frontend/index.php
@@ -11,8 +11,8 @@
   <meta name="keywords" content="">
 
   <!-- Favicons -->
-  <link href="assets/assets/img/favicon.png" rel="icon">
-  <link href="assets/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <!-- <link href="assets/assets/img/favicon.png" rel="icon">
+  <link href="assets/assets/img/apple-touch-icon.png" rel="apple-touch-icon"> -->
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com" rel="preconnect">
