1981925b3091508e602232095cadf96ff6834445
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2025-05-11 05:08:14 +0000

    `Removed and added and removed include statement for predict.php, no other changes`

--- backend/actions/login.php
+++ backend/actions/login.php
@@ -11,7 +11,7 @@ include_once "../settings/connection.php";
 include_once "../functions/send_OTP.php";
 
 // Include the predict function
-include_once "../functions/predict.php"; 
+include_once "../functions/predict.php";
 
 $frontend_url = getenv("FRONTEND_URL") ?: "http://13.60.64.199:3000";
 $backend_url = getenv("BACKEND_URL") ?: "http://13.60.64.199:8080";
@@ -50,10 +50,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $stmt = $pdo->prepare("SELECT OrganizationID, Password, Name FROM Organizations WHERE Email = :Email");
             $stmt->bindParam(":Email", $organizationEmail, PDO::PARAM_STR);
             $stmt->execute();
-    
+
             if ($stmt->rowCount() >= 1) {
                 $row = $stmt->fetch();
-    
+
                 if (password_verify($password, $row['Password'])) {
                     $_SESSION['OrganizationID'] = $row['OrganizationID'];
                     $_SESSION['organization_email'] = $organizationEmail;
@@ -88,7 +88,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     // After login, call the Flask API
                     $headers = [
                         'Content-Type: application/json',
-                        'Organizationid: ' . $organizationID  
+                        'Organizationid: ' . $organizationID
                     ];
 
                     // $ch = curl_init('http://127.0.0.1:5000/get_scores_1');
@@ -140,8 +140,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         echo "The message is: " . htmlspecialchars($message);
     }
-}
- else {
+} else {
     header("Location: $frontend_url/views/login.php?msg=" . urlencode('Invalid request method.'));
     exit();
 };
