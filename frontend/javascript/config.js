4da56cb5992f4f83add02a162c4ac6aafcd6c7ad
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2025-05-11 04:47:49 +0000

    `Update frontend URLs to point to 13.60.64.199 in config.js and search.js`

--- frontend/javascript/config.js
+++ frontend/javascript/config.js
@@ -1,3 +1,3 @@
-export const FRONTEND_URL = 'http://localhost:3000';
-export const BACKEND_URL = 'http://localhost:8080';
-export const ML_API_URL = 'http://localhost:5001';
+export const FRONTEND_URL = 'http://13.60.64.199:3000';
+export const BACKEND_URL = 'http://13.60.64.199:8080';
+export const ML_API_URL = 'http://13.60.64.199:5001';
